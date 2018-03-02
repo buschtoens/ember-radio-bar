2ecbb4cfb653adacab0c593006bc28c7dbb06850
Jan Buschtöns <jan@buschtoens.me>
2018-03-02 07:16:39 +0100

    chore(deps): upgrade to ember-cli@3

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,34 @@ module.exports = {
     'ember/named-functions-in-promises': ['error', { allowSimpleArrowFunction: true }],
     'no-var': 'error',
     'prefer-const': 'error'
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
